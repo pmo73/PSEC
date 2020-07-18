4ae265537a26d54c094ac23a731decff34cf167d
pmo73 <bjoern2.petersen@t-online.de>
2020-07-18 07:17:56 -0400

    Zmap korrigiert

--- Task1/zmap.bash
+++ Task1/zmap.bash
@@ -1,3 +1,54 @@
-kali@kali:~/Documents/PSEC/Task1$ sudo zmap -V
-zmap 2.1.1
-
+kali@kali:~/Documents/git/PSEC/Task1$ sudo zmap --interface=eth0 -p 80 134.60.0.0/23
+Jul 18 07:17:17.921 [WARN] blacklist: ZMap is currently using the default blacklist located at /etc/zmap/blacklist.conf. By default, this blacklist excludes locally scoped networks (e.g. 10.0.0.0/8, 127.0.0.1/8, and 192.168.0.0/16). If you are trying to scan local networks, you can change the default blacklist by editing the default ZMap configuration at /etc/zmap/zmap.conf.
+Jul 18 07:17:17.931 [INFO] zmap: output module: csv
+Jul 18 07:17:17.931 [INFO] csv: no output file selected, will use stdout
+ 0:00 1%; send: 251 0 p/s (4.43 Kp/s avg); recv: 0 0 p/s (0 p/s avg); drops: 0 p/s (0 p/s avg); hitrate: 0.00%
+134.60.1.34
+134.60.1.208
+134.60.1.11
+134.60.1.126
+134.60.1.134
+134.60.1.43
+134.60.1.207
+134.60.1.21
+134.60.1.98
+134.60.1.206
+134.60.1.55
+134.60.1.46
+134.60.1.212
+134.60.1.198
+134.60.1.221
+134.60.1.116
+134.60.1.115
+134.60.1.211
+134.60.1.222
+134.60.1.84
+134.60.1.30
+134.60.1.39
+134.60.1.91
+ 0:01 13%; send: 512 done (8.76 Kp/s avg); recv: 23 22 p/s (21 p/s avg); drops: 0 p/s (0 p/s avg); hitrate: 4.49%
+134.60.1.128                                                                                                                                                                                                                                                                                      
+134.60.1.214                                                                                                                                                                                                                                                                                      
+134.60.1.47                                                                                                                                                                                                                                                                                       
+134.60.1.48                                                                                                                                                                                                                                                                                       
+134.60.1.137                                                                                                                                                                                                                                                                                      
+134.60.1.192                                                                                                                                                                                                                                                                                      
+134.60.1.89                                                                                                                                                                                                                                                                                       
+134.60.1.66                                                                                                                                                                                                                                                                                       
+134.60.1.78                                                                                                                                                                                                                                                                                       
+134.60.1.216                                                                                                                                                                                                                                                                                      
+134.60.1.213                                                                                                                                                                                                                                                                                      
+134.60.1.49                                                                                                                                                                                                                                                                                       
+134.60.1.33                                                                                                                                                                                                                                                                                       
+134.60.1.210                                                                                                                                                                                                                                                                                      
+134.60.1.22                                                                                                                                                                                                                                                                                       
+134.60.1.150                                                                                                                                                                                                                                                                                      
+ 0:02 26%; send: 512 done (8.76 Kp/s avg); recv: 39 15 p/s (18 p/s avg); drops: 0 p/s (0 p/s avg); hitrate: 7.62%                                                                                                                                                                                 
+ 0:03 38%; send: 512 done (8.76 Kp/s avg); recv: 39 0 p/s (12 p/s avg); drops: 0 p/s (0 p/s avg); hitrate: 7.62%                                                                                                                                                                                  
+ 0:04 50%; send: 512 done (8.76 Kp/s avg); recv: 39 0 p/s (9 p/s avg); drops: 0 p/s (0 p/s avg); hitrate: 7.62%                                                                                                                                                                                   
+ 0:05 63% (3s left); send: 512 done (8.76 Kp/s avg); recv: 39 0 p/s (7 p/s avg); drops: 0 p/s (0 p/s avg); hitrate: 7.62%                                                                                                                                                                         
+ 0:06 75% (2s left); send: 512 done (8.76 Kp/s avg); recv: 39 0 p/s (6 p/s avg); drops: 0 p/s (0 p/s avg); hitrate: 7.62%                                                                                                                                                                         
+ 0:07 88% (1s left); send: 512 done (8.76 Kp/s avg); recv: 39 0 p/s (5 p/s avg); drops: 0 p/s (0 p/s avg); hitrate: 7.62%                                                                                                                                                                         
+ 0:08 101% (0s left); send: 512 done (8.76 Kp/s avg); recv: 39 0 p/s (4 p/s avg); drops: 0 p/s (0 p/s avg); hitrate: 7.62%                                                                                                                                                                        
+Jul 18 07:17:27.150 [INFO] zmap: completed                                                                                                                                                                                                                                                        
+kali@kali:~/Documents/git/PSEC/Task1$    
